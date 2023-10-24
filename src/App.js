04860cfc9e46951a834488061ffdf2b7c5c660e6
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-10-24 19:03:57 +0300

    task done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
-
+import DogPics from "./DogPics";
 function App() {
+  const [count, setCount] = useState(0);
   useEffect(() => {
-    console.log("useEffect called");
+    console.log(count);
+    setTimeout((() => setCount(0), 5000));
   });
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)}>Click Me {count}</button>
+      <DogPics count={count} />
+    </>
+  );
 }
 
 export default App;
